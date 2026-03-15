26a84a4f4da446e8634f96f0a1856467037515df
Harsh-Projects-001 <hs2751233@gmail.com>
2026-03-16 00:50:06 +0530

    Experiment 6 uploaded

--- Experiment 6/healthhub/src/main/java/com/example/healthhub/HealthhubApplication.java
+++ Experiment 6/healthhub/src/main/java/com/example/healthhub/HealthhubApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HealthhubApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HealthhubApplication.class, args);
 	}
